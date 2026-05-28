1c7a157785eef2d74beda0b0c026256a7161bc33
ccheerry <alvarocherry0@gmail.com>
2026-05-28 16:32:46 +0200

    feat(solver): pathfinding algorithm

---
+++ srcs/solver/algo.c
@@ -0,0 +1,7 @@
+#include "lem_in.h"
+
+int	algo(t_lem_in *lem)
+{
+	(void)lem;
+	return (0);
+}
---
+++ srcs/solver/paths.c
@@ -0,0 +1,21 @@
+#include "lem_in.h"
+
+t_path	*extract_paths(t_lem_in *lem)
+{
+	(void)lem;
+	return (NULL);
+}
+
+int	select_paths(t_lem_in *lem)
+{
+	(void)lem;
+	return (0);
+}
+
+int	calc_turns(t_path *paths, int num_paths, int num_ants)
+{
+	(void)paths;
+	(void)num_paths;
+	(void)num_ants;
+	return (0);
+}
